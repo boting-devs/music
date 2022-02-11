1b494d21165e5c3bb259e1300bdd311e4bd7f536
Arnavop <arnavkamramalout@gmail.com>
2022-02-11 17:08:22 +0530

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -328,7 +328,7 @@ class Music(Cog, name="music", description="Play some tunes with or without frie
 
         @command()
         async def hello(self,ctx):
-            await ctx.send("hey")
-            
+            await ctx.send_author_embed("hey")
+
 def setup(bot: MyBot):
     bot.add_cog(Music(bot))
