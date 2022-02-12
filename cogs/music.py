8dab64b1bd7c6c55d03666621522d38342897bfe
ooliver1 <oliverwilkes2006@icloud.com>
2022-02-12 21:52:43 +0000

    fix: aaaaaaaaaaaaaaaaaaaa

--- cogs/music.py
+++ cogs/music.py
@@ -550,8 +550,6 @@ class Music(Cog, name="music", description="Play some tunes with or without frie
         except IndexError:
             raise LyricsNotFound()
 
-        a = await ctx.send("`Searching....`")
-
         async with self.bot.session.get(source) as resp:
             txt = await resp.text()
 
