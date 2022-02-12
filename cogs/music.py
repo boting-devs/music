23dbc8e493e0262c19562727fb6c00cec722a557
Arnavop <arnavkamramalout@gmail.com>
2022-02-12 13:52:12 +0530

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -92,15 +92,15 @@ class PlayButon(View):
             await inter.response.edit_message(view=self)
 
     @button(emoji="\U000023ed",style=ButtonStyle.blurple,custom_id="view:next")
-    async def nextbutton(self,ctx:MyContext, button:Button,inter:Interaction):
+    async def nextbutton(self, button:Button,inter:Interaction):
         assert inter.guild is not None
         assert inter.guild.voice_client is not None
         assert isinstance(inter.guild.voice_client, Player)
-        if not ctx.voice_client.queue:
+        if not inter.guild.voice_client.queue:
             return await inter.send("Nothing in queue",ephemeral=True)
 
-        toplay = ctx.voice_client.queue.pop(0)
-        await ctx.voice_client.play(toplay)
+        toplay = inter.guild.voice_client.queue.pop(0)
+        await inter.guild.voice_client.play(toplay)
         await self.playing_embed(toplay)
 
 
