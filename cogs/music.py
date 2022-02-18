db9d740777a3c86a45e23c39378334abdbb0225c
Arnavop <arnavkamramalout@gmail.com>
2022-02-18 16:36:25 +0530

    ah

--- cogs/music.py
+++ cogs/music.py
@@ -127,8 +127,8 @@ async def playing_embed(
     if queue:
         await ctx.send(embed=embed, content="Queued", view=view)
     if length:
-        channel = ctx.author.text.channel
-        await ctx.send(embed=embed,view=view)
+        channel = ctx.channel
+        await channel.send(embed=embed,view=view)
     else:
         await ctx.send(embed=embed, view=view)
 
