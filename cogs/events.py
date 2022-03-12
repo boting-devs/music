5572d61549f104ba3bd64457f11357e5f1dc2091
ooliver1 <oliverwilkes2006@icloud.com>
2022-03-12 18:43:55 +0000

    fix: forgor the other half of the fix

--- cogs/events.py
+++ cogs/events.py
@@ -38,6 +38,9 @@ class Events(Cog):
                     self.bot.listeners[after.channel.id] = {member.id}
                 else:
                     self.bot.listeners[after.channel.id].add(member.id)
+
+                if task := self.bot.listener_tasks.get(member.guild.id):
+                    task.cancel()
             elif before.channel and not after.channel:
                 if before.channel.id in self.bot.listeners:
                     self.bot.listeners.get(before.channel.id, set()).discard(member.id)
