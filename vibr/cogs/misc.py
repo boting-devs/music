b4c5d3bb23c4260befea8eaf0e7d5f499f6fcf32
Arnavop <72515652+Arnavop@users.noreply.github.com>
2022-08-04 21:57:00 +0530

    Update misc.py

--- vibr/cogs/misc.py
+++ vibr/cogs/misc.py
@@ -107,7 +107,7 @@ class Misc(Cog, name="misc", description="Meta commands about the bot!"):
         embed = Embed(title="Vibr's Notifications",description="**__List of vibr's important announcements/notifications__**",color=self.bot.color)
         stop = 10
         for (index,value) in enumerate(notifs[:stop],start=1):
-            embed.add_field(name=f'{index}) **{value["notification"]}**',value=f'{value["datetime[:10]"]}',inline=False)
+            embed.add_field(name=f'{index}) **{value["notification"]}**',value=f'{value["datetime"][:10]}',inline=False)
         embed.set_footer(
             text=f"Requested by {ctx.author.name}",
             icon_url=ctx.author.display_avatar.url,
