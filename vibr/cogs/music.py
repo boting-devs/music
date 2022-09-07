381261d07208eb86b62ec2e47a0cbd85ff60bb67
Arnavop <arnavkamramalout@gmail.com>
2022-09-08 00:16:26 +0530

    success is near #5

--- vibr/cogs/music.py
+++ vibr/cogs/music.py
@@ -591,7 +591,6 @@ class Music(Cog, name="music", description="Play some tunes with or without frie
             return await inter.send_author_embed("No tracks found")
 
         result = result[0]
-        player.queue.insert(0,result)
         await player.play(track=result)
         await playing_embed(result)
 
