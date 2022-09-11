4635f3ac6e968a360d911a7ebce966a65d117732
ooliver1 <oliverwilkes2006@icloud.com>
2022-09-11 20:46:00 +0100

    fix: use port for beta

--- vibr/config.py
+++ vibr/config.py
@@ -1,9 +1,12 @@
 from os import getenv
 
-if uri := getenv("DB_URI"):
-    db_url = uri  # dev
-else:
-    db_name = "vibr"  # prod
+if uri := getenv("DB_URI"):  # dev
+    db_url = uri
+elif host := getenv("DB_HOST"):  # beta
+    db_host = host
+    db_port = getenv("DB_PORT")
+else:  # prod
+    db_name = "vibr"
 
 name = "vibr"
 colors = [0xFF00E1, 0xDA00FF, 0x8000FF, 0x2500FF, 0x008FFF]
