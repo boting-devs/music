a169ee7269018e7d5d6a9cece7e545785106f17d
Oliver Wilkes <oliverwilkes2006@icloud.com>
2022-10-03 12:52:07 +0100

    remove unused placeholder

--- vibr/cogs/stats.py
+++ vibr/cogs/stats.py
@@ -70,7 +70,7 @@ class Stats(Cog):
                  commands)
 
             VALUES (
-                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10,
+                $1, $2, $3, $4, $5, $6, $7, $8, $9,
                 (SELECT SUM(amount) FROM commands)
             )
             """,
