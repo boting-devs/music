c98ead01a957bd221fc1cde961bc310dd5e5c478
Arnavop <arnavkamramalout@gmail.com>
2023-04-23 17:33:59 +0530

    added nightcore and fixed rotate

---
+++ vibr/exts/filters/nightcore.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from botbase import CogBase, MyInter
+from mafic import Timescale, Filter
+from nextcord import slash_command
+
+from vibr.bot import Vibr
+from vibr.checks import is_connected_and_playing
+from vibr.embed import Embed
+
+if TYPE_CHECKING:
+    from vibr.player import Player
+
+
+class Nightcore(CogBase[Vibr]):
+    @slash_command(name="nightcore",dm_permission=False)
+    @is_connected_and_playing
+    async def nightcore(self,inter:MyInter) -> None:
+        """A funny filter. Just Try it out!"""
+
+        assert inter.guild is not None and inter.guild.voice_client is not None
+
+        player :Player = inter.guild.voice_client #pyright :ignore
+
+
+        if await player.has_filter("nightcore"):
+            await player.remove_filter("nightcore",fast_apply=True)
+            embed = Embed(title="Nightcore Filter Deactivated")
+
+        else:
+            night = Timescale(speed=1.25,pitch=1.3)
+            nightcore_filter = Filter(timescale=night)
+            await player.add_filter(nightcore_filter,label="nightcore",fast_apply=True)
+            embed =Embed(title="Nightcore Filter activated")
+        
+        await inter.send(embed=embed)
+            
+
+def setup(bot: Vibr) -> None:
+    bot.add_cog(Nightcore(bot))
--- vibr/exts/filters/rotate.py
+++ vibr/exts/filters/rotate.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING ,Optional
 
 from botbase import CogBase, MyInter
-from mafic.filter import Rotation
+from mafic import Rotation , Filter
 from nextcord import slash_command ,Range
 
 from vibr.bot import Vibr
@@ -12,6 +12,7 @@ from vibr.embed import Embed
 
 if TYPE_CHECKING:
     from vibr.player import Player
+
 
 class Rotate(CogBase[Vibr]):
     @slash_command(name="rotate",dm_permission=False)
@@ -32,23 +33,30 @@ class Rotate(CogBase[Vibr]):
         assert inter.guild is not None and inter.guild.voice_client is not None
 
         player: Player = inter.guild.voice_client  # pyright: ignore
+        
 
         if await player.has_filter("rotate") and passed_custom:
             await player.remove_filter("rotate")
+            rotate = Rotation(rotation_hz=frequency)
+            rotate_filter = Filter(rotation=rotate)
             await player.add_filter(
-                Rotation(rotation_hz=frequency),label="rotate",fast_apply=True
+                rotate_filter,label="rotate",fast_apply=True
             )
-            embed = Embed("Rotation filter modified")
+            embed = Embed(title="Rotation filter modified")
 
         elif await player.has_filter("rotate"):
             await player.remove_filter("rotate",fast_apply=True)
-            await inter.send("Rotation filter Deactivated")
+            embed = Embed(title="Rotation filter Deactivated")
 
         else:
+            rotate = Rotation(rotation_hz=frequency)
+            rotate_filter = Filter(rotation=rotate)
             await player.add_filter(
-                Rotation(rotation_hz=frequency),label="rotate",fast_apply=True
+                rotate_filter,label="rotate",fast_apply=True
             )
-            await inter.send("Rotation filter activated")
+            embed= Embed(title="Rotation filter activated")
+
+        await inter.send(embed=embed)
 
 def setup(bot: Vibr) -> None:
     bot.add_cog(Rotate(bot))
