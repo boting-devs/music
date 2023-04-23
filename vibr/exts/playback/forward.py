8cc828971a7260e2a8ac9598612a5a0e3e519381
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-04-23 16:41:21 +0100

    and again

--- vibr/exts/playback/forward.py
+++ vibr/exts/playback/forward.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from time import gmtime, strftime
+from typing import TYPE_CHECKING
 
 from botbase import CogBase, MyInter
 from nextcord import slash_command
@@ -10,6 +9,7 @@ from nextcord import slash_command
 from vibr.bot import Vibr
 from vibr.checks import is_connected_and_playing
 from vibr.embed import Embed
+
 from ._errors import NotInRange
 
 if TYPE_CHECKING:
@@ -19,9 +19,9 @@ if TYPE_CHECKING:
 class Forward(CogBase[Vibr]):
     @slash_command(dm_permission=False)
     @is_connected_and_playing
-    async def forward(self,inter:MyInter , seconds:int) -> None:
+    async def forward(self, inter: MyInter, seconds: int) -> None:
         """Seeks forward in the current song by an amount
-        
+
         seconds:
             The amount to seek forward by in seconds.
         """
@@ -32,13 +32,14 @@ class Forward(CogBase[Vibr]):
         time_left = player.current.length - c
         # s -> ms
         amount = c + (seconds * 1000)
-        if amount> time_left:
+        if amount > time_left:
             raise NotInRange
         # Format the time into a human readable format.
-        current = strftime("%H:%M:%S", gmtime((amount // 1000)))
+        current = strftime("%H:%M:%S", gmtime(amount // 1000))
         await player.seek(amount)
         embed = Embed(title=f"Position seeked to {current}")
         await inter.send(embed=embed)
+
 
 def setup(bot: Vibr) -> None:
     bot.add_cog(Forward(bot))
