7449cb9b3d288d7a37fea24755ad3040443b6ad9
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-05-18 15:26:52 +0100

    refactor: change database for spotify

--- vibr/db/user.py
+++ vibr/db/user.py
@@ -2,14 +2,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-from async_spotify.authentification import SpotifyAuthorisationToken
 from botbase import BaseMeta
-from ormar import BigInteger, DateTime, Model, String
-
-if TYPE_CHECKING:
-    from datetime import datetime
+from ormar import BigInteger, Model, Text
 
 
 class User(Model):
@@ -17,21 +11,4 @@ class User(Model):
         tablename = "users"
 
     id: int = BigInteger(primary_key=True, autoincrement=False)
-    spotify_access_token: str | None = String(max_length=255, nullable=True)
-    spotify_refresh_token: str | None = String(max_length=255, nullable=True)
-    spotify_activation_time: datetime | None = DateTime(timezone=True, nullable=True)
-
-    @property
-    def spotify(self) -> SpotifyAuthorisationToken | None:
-        if (
-            self.spotify_access_token is None
-            or self.spotify_refresh_token is None
-            or self.spotify_activation_time is None
-        ):
-            return None
-
-        return SpotifyAuthorisationToken(
-            access_token=self.spotify_access_token,
-            refresh_token=self.spotify_refresh_token,
-            activation_time=self.spotify_activation_time.timestamp(),
-        )
+    spotify_id: str = Text()
