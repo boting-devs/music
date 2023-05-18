9bd5bb4ae28f8330c4fc7991f82d4d4db44ce220
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-05-18 15:29:04 +0100

    refactor: remove api

--- api/__main__.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import uvloop
-
-uvloop.install()
-uvicorn.run(
-    "api.app:app",
-    host="0.0.0.0",  # noqa: S104
-    port=8000,
-)
--- api/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-until nc -w 1 postgres 5432; do sleep 0.1; done
-poetry run alembic upgrade head
-poetry run task start
