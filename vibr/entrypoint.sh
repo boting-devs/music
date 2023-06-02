72998acf07dc18e3d64a0d247687499b3d729e83
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-06-02 10:19:33 +0100

    b

--- vibr/entrypoint.sh
+++ vibr/entrypoint.sh
@@ -1,7 +1,7 @@
 until nc -w 1 postgres 5432; do sleep 0.1; done
 export HOST_IP=$(ip route show | awk '/default/ {print $3}')
 poetry run piccolo migrations forwards all
-nc -w 1 lavalink 6969
-printf '%d\n' $?
+nc -w 1 lavalink 6969 || EXIT_CODE=$?
+echo $EXIT_CODE
 until nc -w 1 $HOST_IP 6969 || (nc -w 1 lavalink 6969); do sleep 0.1; done
 poetry run task start
