f662d8f969cc4f68f0dba54b7c972eb0a0d47c15
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-08-01 16:25:09 +0100

    refactor: reduce cache sizes (#105)
    Modified gateway-proxy (via twilight) to trim the fields we store about
    users (inside members), members (bot member), roles (permissions),
    channels (permissions) and guilds (voice states).
    
    I have not fully tested this with commands, playing music, making sure
    permission checks work, e.t.c.

--- vibr/__main__.py
+++ vibr/__main__.py
@@ -8,7 +8,7 @@ from .bot import Vibr
 from .log import setup_logging
 
 bot = Vibr()
-
+__import__("logging").getLogger("nextcord").setLevel("DEBUG")
 setup_logging()
 uvloop.install()
 bot.run(environ["TOKEN"])
